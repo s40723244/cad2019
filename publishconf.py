b5e0f320a476e06261061cdd303e32c66ac92cf4
s40723244 <40723244@gm.nfu.edu.tw>
2019-12-08 01:21:55 +0800

    week8-blog-text

--- publishconf.py
+++ publishconf.py
@@ -30,7 +30,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "disqus_ikOLamsBC9"
+DISQUS_SITENAME = "cad2019"
 DISQUS_DISPLAY_COUNTS = False
 #GOOGLE_ANALYTICS = ""
 
